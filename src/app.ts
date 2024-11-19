bcba3491a0a348318e32c7c9a1044731b4a7f124
Antonio Sotnik <toxanetoxa@gmail.com>
2024-11-19 17:47:04 +0300

    make structure project

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,9 @@ import express, { Request, Response } from 'express';
 const app = express();
 const PORT = 3000;
 
+// Middleware для обработки JSON
+app.use(express.json());
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, World!');
 });
